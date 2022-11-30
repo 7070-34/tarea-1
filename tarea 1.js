4815e17290bf114eb74eadf304da3839682fdf0d
nadia <znadia486@gmail.com>
2022-11-29 21:00:42 -0300

    Trabajo

---
+++ tarea 1.js
@@ -0,0 +1,151 @@
+﻿let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Cambia solo el código debajo de esta línea
+a *= 5;
+b *= 3;
+c *= 10;
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Cambia solo el código debajo de esta línea
+a -= 6;
+b -= 15;
+c -= 1;
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+// Cambia solo el código debajo de esta línea
+const myName = "Nadia";
+const myStr = "My name is " + myName + "and I am well!";
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Cambia esta línea
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Cambia esta línea
+// Cambia el código debajo de esta línea
+const someAdjective = "awesome!";
+let myStr = "Learning to code is ";
+myStr += someAdjective ;
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Cambia solo el código debajo de esta línea
+a += 12;
+b += 9;
+c += 7;
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Cambia solo el código debajo de esta línea
+a /= 12;
+b /= 4;
+c /= 11;
+// Configuración
+let myStr = "Jello World";
+
+// Cambia solo el código debajo de esta línea
+myStr = "Hello World"; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+// Configuración
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+firstLetterOfLastName = lastName[0]; ; // Cambia esta línea
+// Configuración
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+lastNameLength = lastName.length;
+const myStr = "This is the start. " + "This is the end."; // Cambia esta lÃ­nea
+// Cambia solo el código debajo de esta línea
+const myArray = [["Argentina", 2], ["Mexico", 0]];
+// Cambia solo el código debajo de esta línea
+const myArray = ["jelly",9];
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Cambia esta línea
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Cambia esta línea
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const thirdLetterOfLastName = lastName[2]; // Cambia esta línea
+const FCC = "freeCodeCamp"; // Cambia esta línea
+let fact = "is cool!"; // Cambia esta línea
+fact = "is awesome!";
+console.log(FCC, fact); // Cambia esta línea
+// Declaración de variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Asignación de variables
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
+// Cambia solo el código debajo de esta línea
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Cambia solo el código encima de esta línea
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+var myFirstName = "Nadia";
+var myLastName = "Zacarias";
+var a = 9;
+// ConfiguraciÃ³n
+var a;
+a = 7;
+var b;
+b = a;
+
+// Cambia solo el cÃ³digo debajo de esta lÃ­nea
+// ConfiguraciÃ³n
+var a;
+a = 7;
+
+// Cambia solo el cÃ³digo debajo de esta lÃ­nea
+var myName;
+//comentario//
+/*comentario*/
+const ourDecimal = 5.7;
+
+// Cambia solo el código debajo de esta línea
+const myDecimal = 4.2;
+let myVar = 11;
+
+// Cambia solo el código debajo de esta línea
+myVar--;
+const quotient = 4.4 / 2.0; // Cambia esta línea
+const quotient = 66 / 33;
+const remainder = 11 % 3;
+let myVar = 87;
+
+// Cambia solo el código debajo de esta línea
+myVar++;
+const product = 8 * 10;
+const product = 2.0 * 2.5;
+const difference = 45 - 33;
+const sum = 10 + 10;
